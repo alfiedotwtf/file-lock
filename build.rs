ff24cbeddce14c53b7d6ab539dc93ce7fef7b464
Alfie John <alfie@alfie.wtf>
2015-04-19 12:45:45 +1000

    cargo: fighting build depenencies

--- build.rs
+++ build.rs
@@ -1,20 +1,5 @@
-use std::env;
-use std::path::Path;
-use std::process::Command;
+extern crate gcc;
 
 fn main() {
-  let out_dir = env::var("OUT_DIR").unwrap();
-
-  Command::new("gcc")
-    .args(&["src/lock.c", "-c", "-fPIC", "-o"])
-    .arg(&format!("{}/lock.o", out_dir))
-    .status().unwrap();
-
-  Command::new("ar")
-    .args(&["crus", "liblock.a", "lock.o"])
-    .current_dir(&Path::new(&out_dir))
-    .status().unwrap();
-
-    println!("cargo:rustc-link-search=native={}", out_dir);
-    println!("cargo:rustc-link-lib=static=lock");
+  gcc::compile_library("liblock.a", &["src/lock.c"]);
 }
